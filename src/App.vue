707b3d6f6f18094080f83ec50feadb0524f8e020
aditiyadwiramadani88 <aditiyadwiramadani88@gmail.com>
2023-08-09 18:51:19 +0700

    Vue admin templete

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+<template>
+  <div class="container mx-auto">
+    <HelloWorld msg="Vite + Vue" data="dunia" />
+  </div>
+
+</template>
