3f95a44aaffc039ff88c1966200a8ee1241d6d68
aditiyadwiramadani88 <aditiyadwiramadani88@gmail.com>
2023-08-10 12:07:13 +0700

    Vuejs Simple Template Admin

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Dasboard Vuejs + Tailwind Css
+- simple admin dasboard using vuejs and tailwind css 
+- chart js 
+
+## Install 
+```sh
+npm install 
+```
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
+// import { Chart } from 'chart.js';
 import HelloWorld from './components/HelloWorld.vue'
-
+import Sidebar from './components/Sidebar.vue';
+import Chart from './components/Chart.vue';
 </script>
 <template>
-  <div class="container mx-auto">
-    <HelloWorld msg="Vite + Vue" data="dunia" />
-  </div>
 
+  <Sidebar />
+<!-- <Chart /> -->
+  <!-- <div class="container mx-auto font-semibold">
+    <HelloWorld msg="Vite + Vue" data="dunia" />
+  </div> -->
 </template>
